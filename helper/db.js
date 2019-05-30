effce7856d340f19694a90cfadc489bd684b8987
Recep Ali GENÇ <recepaligenc@gmail.com>
2019-05-30 15:05:50 +0300

    updated test script

--- helper/db.js
+++ helper/db.js
@@ -2,7 +2,7 @@ var mongooseLib = require('mongoose');
 mongooseLib.set('useCreateIndex', true);
 
 module.exports = ()=>{
-    mongooseLib.connect('mongodb://udemyuser:vJvkm6GzHKUH@127.0.0.1:27017/udemy', {useNewUrlParser: true, useFindAndModify: false });
+    mongooseLib.connect('mongodb://udemyuser:vJvkm6GzHKUH@37.247.111.142:29111/udemy', {useNewUrlParser: true, useFindAndModify: false });
     mongooseLib.connection.on('open', ()=>{
         console.log('connection created');
     });
